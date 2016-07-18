7094dda5232bb277b5e2ca451cabd614b713b139
Ian321 <ignaz.k@live.de>
2016-07-18 22:53:58 +0200

    Update for the header

--- multi/index.php
+++ multi/index.php
@@ -54,6 +54,7 @@
 		Have fun with it :)
 		-->
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Multistream</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 		<style>
